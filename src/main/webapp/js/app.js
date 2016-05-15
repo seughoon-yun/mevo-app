82342747e93e9a95f90205d686187fc2be92f8d0
Finn Lawrence <finn.lawrence@gmail.com>
2016-05-15 13:12:02 +1200

    Changed app colours

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -93,6 +93,6 @@ function setState(state) {
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-        window.location = "http://app.mevo.co.nz";
+        window.location = "http://mevo.co.nz";
     });
 }
--- src/main/webapp/js/site.js
+++ src/main/webapp/js/site.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
     $(".button-collapse").sideNav();
-    $('.side-nav a').click(function() {
-        $('.side-nav a').removeClass("active");
-        $(this).addClass("active");
-        var targetPane = $(this).attr('href');
-        $('.app-pane').removeClass("active");
-        $(targetPane).addClass("active");
+    $('.side-nav li a').click(function() {
+        
+        // If it's an app pane activator, select the right pane
+        if ($(this).hasClass("app-pane-activator")) {
+            $('.app-pane-activator').removeClass("active");
+            $(this).addClass("active");
+            var targetPane = $(this).attr('href');
+            $('.app-pane').removeClass("active");
+            $(targetPane).addClass("active");
+        }
+        
+        // For all links, close the side nav if we are on mobile
         if ($(window).width() < 993) {
-            console.log("closing nav");
             $('.button-collapse').sideNav('hide');
         }
     });
